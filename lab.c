0033028a15d0732c533d8398381d851a4d45e78f
Sami-Jouamaa <122917968+Sami-Jouamaa@users.noreply.github.com>
2024-01-10 13:06:59 -0500

    Add files via upload

---
+++ lab.c
@@ -0,0 +1,85 @@
+/* INF2610 - TP1
+/ Matricule 1 : 2247399
+/ Matricule 2 : 2141605
+*/
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h> 
+#include <stdbool.h>
+
+struct Wheel{
+    int id;
+    bool isRearWheel;
+};
+
+struct Wing{
+    int id[9];
+};
+
+struct Plane{
+        char idPlane[10];
+        char planeType[10];
+        bool isAvailable;
+        Wheel wheels[7];
+        Wings wings[2];
+};
+
+int main(int argc, char** argv) {
+    printf("Hello\n");
+    /* Remove comment once the code is completed for the given section to test */
+
+    
+
+
+    int id = 1;
+
+
+    /* PARTIE 2 - [10 points] */
+
+    /* Create wheel - [2 points] */
+    /*
+    Wheel[] wheels;
+    wheels = createWheels(id); 
+    
+
+    /* Create wing - [4 points] */
+    /*
+    long longId = 1;
+    Wing[] wings;
+    wings = createWings(longId);
+    */
+
+    /* Create plane - [4 points] */
+    /*
+    int numberOfPlanes = 3;
+    Plane* planes = malloc(sizeof(Plane) * numberOfPlanes);
+    createPlanes(planes, *id, 3);
+    */
+
+    /* PARTIE 3 - [6 points] */
+
+    /* Set availabilities - [1 point] */
+    /*
+    Plane plane = planes[0];
+    setAvailability(plane, true);
+    */
+
+    /* Get available planes - [1 point] */
+    /*
+    getAvailablePlanes(planes, numberOfPlanes);
+    */
+
+    /* Classify planes - [2 points] */
+    /*
+    Plane plane = planes[1];
+    setPlaneType(plane);
+    */
+
+    /* Return type specific planes - [2 points] */
+    /*
+    char planeType[] = "Small";
+    getPlanesByType(planes, planeType,numberOfPlanes);
+    */
+}
